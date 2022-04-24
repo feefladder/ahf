ed84a529402677664e79358131a7b86ba5e8fd20
Joeperdefloep <ikbendeleukstethuis@gmail.com>
2022-04-24 18:46:05 +0200

    typo

--- Assets/Scripts/Decision_Handler.cs
+++ Assets/Scripts/Decision_Handler.cs
@@ -167,7 +167,7 @@ public class Decision_Handler : MonoBehaviour
         }
         
     }
-    public  void RemoveeSchool(int grade)
+    public  void RemoveSchool(int grade)
     {
         if (grade == 0)
         {
