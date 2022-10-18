3a038d1e947a6d2d86e55fe584933e337532988b
Fee <freakyfee@proton.me>
2022-10-18 14:58:03 +0200

    gut not in debug mode

--- gut.sh
+++ gut.sh
@@ -24,4 +24,4 @@ fi
 # curl -s -S ${GODOT_URL} --output ${GODOT_ZIP}
 unzip -q ${GODOT_ZIP}
 chmod +x ${GODOT_BIN}
-godot -d -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
+godot -s --path $GITHUB_WORKSPACE addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
