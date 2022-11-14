525c5fd60a01eeda3f53df7e0458cb91010b898d
Fee <freakyfee@proton.me>
2022-11-14 08:54:21 +0100

    small fix in stat.gd

--- scripts/UI/stat.gd
+++ scripts/UI/stat.gd
@@ -8,5 +8,8 @@ func _ready() -> void:
     $Title.text = tr(type)
 
 func _on_stat_changed(which: String, new_amount: float) -> void:
+    if not which is String:
+        return
+
     if which == type:
         $Amount.text = String(new_amount) + unit
